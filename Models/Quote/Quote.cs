e73881650c0305a86e131c2e7aa914a5786470c9
Dale Johnson <dale.johnson@marketingradar.com>
2022-12-15 09:55:39 +0000

    Add project files.

---
+++ Models/Quote/Quote.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestWebsite.Models.Quote
+{
+    public class Quote
+    {
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Contact Name")]
+        public string ContactName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Total Size (m2)")]
+        public double TotalSize { get; set; }
+
+        [Display(Name = "Total Rebuild Cost (£)")]
+        public decimal TotalRebuildCost { get; set; }
+
+        [Display(Name = "Flat Roof Cover needed?")]
+        public bool IsFlatRoofCover { get; set; }
+
+        [Display(Name = "Flat Roof Cover")]
+        public int FlatRoofCover { get; set; }
+    }
+}
